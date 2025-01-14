587fc59b90decd4ff771f112900feb8926c28758
Mukesh Kumar Ray <123766305+Mukesh-Kumar-Ray@users.noreply.github.com>
2025-01-15 04:50:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:4001",
+        target: "https://web-chat-api-three.vercel.app",
         changeOrigin: true,
       },
     },
